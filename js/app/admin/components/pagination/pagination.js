e56f89b696cb4ac8a233105a7f9b09199f450464
mshekera <max.shekera@gmail.com>
2014-10-14 21:03:36 +0300

    more or less complted this freakin` pagination

--- js/app/admin/components/pagination/pagination.js
+++ js/app/admin/components/pagination/pagination.js
@@ -1,28 +1,70 @@
 import 'can/'
-import _ from 'lodash'
 import appState from 'appState'
 
 can.Component.extend({
 	tag: 'pagination',
 	template: can.view('/js/app/admin/components/pagination/views/pagination.stache'),
-	scope: {
-		page: 1,
-		pages: 1,
-		pagesArray: function () {
-			return _.range(1, this.attr('pages') + 1);
+	scope: new can.Map({
+		blockBefore: false,
+		blockAfter: false,
+		pages: function () {
+			return  Math.ceil(this.attr('count') / this.attr('limit'));
+		},
+		tryAdd: function (array, method, pages, value) {
+			if(value > 1 && value < pages) {
+				array[method](value);
+			}
+		}
+	}),
+	helpers: {
+		pagesList: function () {
+			var range = [],
+				pages = this.attr('pages').call(this),
+				page = this.attr('page');
+
+			this.tryAdd(range, 'push', pages, page);
+
+			if(page === 1 || page === pages) {
+				var method = page === 1 ? 'push' : 'unshift';
+				var step = page === 1 ? 1 : -1;
+
+				this.tryAdd(range, method, pages, page + step);
+				this.tryAdd(range, method, pages, page + step * 2);
+			} else {
+				this.tryAdd(range, 'unshift', pages, page - 1);
+				this.tryAdd(range, 'push', pages, page + 1);
+			}
+
+			var result = '';
+			for(var i = 0, len = range.length; i < len; i++) {
+				result += '<li class="pn-page';
+				if (page === range[i]) {
+					result += ' active';
+				}
+				result +='" data-page="' + range[i] + '"><a href="#">' 
+										 + range[i] + '</a></li>';
+			}
+
+			this.attr('blockBefore', page > 3 || pages === 4);
+			this.attr('blockAfter', page < pages - 2 || pages === 4);
+			return result;
 		}
 	},
 	events: {
+		'.pn-next click': function (el) {
+			if(el.hasClass('disabled')) return;
+			var page = this.scope.attr('page');
+			this.scope.attr('page', ++page);
+		},
 
+		'.pn-prev click': function (el) {
+			if(el.hasClass('disabled')) return;
+			var page = this.scope.attr('page');
+			this.scope.attr('page', --page);
+		},
 
-		giveAwayData: function (results = false) {
-			try {
-			    this.scope.attr("visible", false);
-			    this.scope.attr('fn').exec(results);
-			} catch(e) {
-				console.error(e);
-				console.error("`articlefilter` component seems do not have callback argument (fn) or it has no function in `exec` property");
-			}
+		'.pn-page click': function (el) {
+			this.scope.attr('page', el.data('page'));
 		}
 	}
 });
