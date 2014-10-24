94e1e837e7b7072af07aa234de67e945719382bf
HydraOrc <hydra0@bigmir.net>
2014-10-24 21:55:16 +0300

    socials for article

--- js/app/user/helpers.js
+++ js/app/user/helpers.js
@@ -26,7 +26,7 @@ window.jadeTemplate = {
 };
 
 window.requestAnimFrame = (function() {
-	return  window.requestAnimationFrame	 || 
+	return	window.requestAnimationFrame	 || 
 		window.webkitRequestAnimationFrame || 
 		window.mozRequestAnimationFrame || 
 		window.oRequestAnimationFrame || 
@@ -38,3 +38,21 @@ window.requestAnimFrame = (function() {
 			}, 1000 / 30);
 		};
 })();
+
+// facebook
+
+(function(d, s, id) {
+	var js, fjs = d.getElementsByTagName(s)[0];
+	if (d.getElementById(id)) return;
+	js = d.createElement(s); js.id = id;
+	js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.0";
+	fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+// twitter
+
+!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
+
+// vk
+
+VK.init({apiId: 4603722, onlyWidgets: true});
--- js/app/user/modules/poll/poll.js
+++ js/app/user/modules/poll/poll.js
@@ -1,3 +1,31 @@
 import Controller from 'controller'
 
-export default Controller.extend();
+export default Controller.extend(
+	{
+		defaults: {
+			
+		}
+	}, {
+		variables: function() {
+			this.id = this.options.id != "poll" ? this.options.id : "undefined";
+		},
+		
+		plugins: function() {
+			var vk_options = {
+					type: "mini",
+					height: 20
+				},
+				ok_options = "{width:145,height:20,st:'rounded',sz:20,ck:1}";
+			
+			VK.Widgets.Like("vk_top_poll_" + this.id, vk_options);
+			VK.Widgets.Like("vk_bottom_poll_" + this.id, vk_options);
+			
+			OK.CONNECT.insertShareWidget("ok_top_poll_" + this.id, document.URL, ok_options);
+			OK.CONNECT.insertShareWidget("ok_bottom_poll_" + this.id, document.URL, ok_options);
+		},
+		
+		after_init: function(data) {
+			
+		}	
+	}
+);
