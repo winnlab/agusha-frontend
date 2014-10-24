f33f44b0fb3223d0c985ccda961971ea96c8ba2d
mshekera <max.shekera@gmail.com>
2014-10-24 22:17:03 +0300

    Partial slider implementation

--- js/app/common/components/slider/slider.js
+++ js/app/common/components/slider/slider.js
@@ -1,15 +1,51 @@
+import _ from 'lodash'
+
+import 'css/common/components/slider.css!'
+
 export default can.Component.extend({
   tag: "slider",
   template: can.view('/js/app/common/components/slider/views/slider.stache'),
   scope: {
-    message: 'HELLO, I`M THE SLIDER!',
     index: '@',
-    count: 0
+    position: 0,
+    timeout: 5000,
+    shift: function (back) {
+      var count = this.attr('gallery').images.attr('length'),
+          pos = this.attr('position');
+
+      can.batch.start();
+
+      pos += back ? -1 : 1;
+
+      if (pos < 0) {
+        pos = count - 1;
+      }
+
+      if (pos >= count) {
+        pos = 0;
+      }
+
+      this.attr('position', pos);
+
+      can.batch.stop();
+
+      _.delay(this.shift.bind(this), this.attr('timeout'));
+    }
   },
   events: {
-    click: function () {
-      this.scope.attr('count', this.scope.attr('count') + 1);
-      this.scope.attr("message", "DON`T TOUCH ME YOU ... !!!" );
+    init: function () {
+      console.log('slider initing')
+
+      var images = this.scope.attr('images'),
+          newGallery = _.where(images, {_id: this.scope.attr('index')});
+      
+      if (!newGallery[0]) {
+        throw new Error('Unknown gallery ID in slider: ' + this.scope.attr('index'));
+      }
+
+      this.scope.attr('gallery', newGallery[0]);
+
+      _.delay(this.scope.shift.bind(this.scope), this.scope.attr('timeout'));
     }
   }
 });
