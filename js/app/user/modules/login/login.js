e5e563146136c246359209b624f2e7862e70c9b4
rastopyr <rastopyr@gmail.com[C>
2014-11-01 17:17:58 +0200

    Add login and registration

--- js/app/user/modules/login/login.js
+++ js/app/user/modules/login/login.js
@@ -1,4 +1,16 @@
 import Controller from 'controller'
+import user from 'js/app/user/modules/user/user'
+
+var ViewModel = can.Map.extend({
+	define: {
+		email: {
+			value: null
+		},
+		password: {
+			value: null
+		}
+	}
+});
 
 export default Controller.extend(
 	{
@@ -7,7 +19,46 @@ export default Controller.extend(
         }
     }, {
 		after_init: function(data) {
-			
+			var registration = $('#login'), html;
+
+			if(!registration.length) {
+				html = jadeTemplate.get('user/login/content');
+			} else {
+				html = registration.html();
+			}
+
+			can.view.mustache('signin', html);
+
+			this.data = new ViewModel();
+
+			$('#login').html(can.view('signin', this.data));
+		},
+		'.login_form .done click': function(el, ev) {
+			ev.preventDefault();
+			var data;
+
+			ev.preventDefault();
+
+			data = this.data;
+
+			can.ajax({
+				url: '/login?ajax=true',
+				method: 'POST',
+				data: data.serialize(),
+				success: function(response) {
+					data.attr({
+						email: null,
+						password: null
+					});
+
+					user.attr('user', response.message.user);
+
+					can.route.attr({module: 'profile'});
+				},
+				error: function () {
+					alert('Произошла ошибка. Пожалуйста, обратитесь к администратору');
+				}
+			});
 		}
     }
 );
