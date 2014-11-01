3e43c6c97aa6f8a030627919be2ea5a8ca07e2be
rastopyr <rastopyr@gmail.com[C>
2014-11-01 13:16:36 +0200

    Add some changes to registrations logick

--- js/app/user/modules/registration/registration.js
+++ js/app/user/modules/registration/registration.js
@@ -1,5 +1,22 @@
 import Controller from 'controller'
 
+var ViewModel = can.Map.extend({
+	define: {
+		email: {
+			value: null
+		},
+		firstname: {
+			value: null
+		},
+		lastname: {
+			value: null
+		},
+		password: {
+			value: null
+		}
+	}
+});
+
 export default Controller.extend(
 	{
         defaults: {
@@ -7,7 +24,45 @@ export default Controller.extend(
         }
     }, {
 		after_init: function(data) {
-			
+			var registration = $('#registration'), html;
+
+			if(!registration.length) {
+				html = jadeTemplate.get('user/registration/content');
+			} else {
+				html = registration.html();
+			}
+
+			can.view.mustache('reg', html);
+
+			this.data = new ViewModel();
+
+			$('#registration').html(can.view('reg', this.data));
+		},
+		'.registration_form .done click': function(el, ev) {
+			var user;
+
+			ev.preventDefault();
+
+			user = this.data;
+
+			can.ajax({
+				url: '/registration?ajax=true',
+				method: 'POST',
+				data: user.serialize(),
+				success: function(data) {
+					alert('Спасибо за регистрацию');
+
+					user.attr({
+						email: null,
+						firstname: null,
+						lastname: null,
+						password: null
+					});
+				},
+				error: function () {
+					alert('Произошла ошибка. Пожалуйста, обратитесь к администратору');
+				}
+			});
 		}
     }
 );
