b42c8fa12c75cde1545e726ad89817e484f91e9d
Maxim Shekera <max.shekera@gmail.com>
2014-11-02 18:47:25 +0200

    Made age/theme multiple in consultations (like in article)

--- js/app/admin/modules/consultations/consultation.js
+++ js/app/admin/modules/consultations/consultation.js
@@ -20,61 +20,50 @@ export default Edit.extend({
         form: '.setConsultation'
     }
 }, {
-	init: function () {
-		_.extend(this.options, options);
+	init: function (element, options) {
+        var self = this;
 
-        var options = this.options,
-            data = {
-                langs: appState.attr('langs')
-            };
+		_.extend(self.options, options);
 
-        this.ensureObject(options.doc, 'age');
-        this.ensureObject(options.doc, 'answer', []);
-        this.ensureObject(options.doc, 'theme');
-        this.ensureObject(options.doc, 'type');
+        var options = self.options;
+
+        self.module = new can.Map({
+            langs: appState.attr('langs')
+        });
+
+        self.ensureObject(options.doc, 'age');
+        self.ensureObject(options.doc, 'answer', []);
+        self.ensureObject(options.doc, 'theme');
+        self.ensureObject(options.doc, 'type');
         
-        data[options.moduleName] = options.doc;
+        self.module.attr(options.moduleName, options.doc);
 
-        data['ages'] = options.ages;
-        data['themes'] = options.themes;
-        data['types'] = options.types;
+        self.module.attr('ages', options.ages);
+        self.module.attr('themes', options.themes);
+        self.module.attr('types', options.types);
 
-        var observables = [
-            {name: 'ageTitle', value: null},
-            {name: 'ageFixture', value: null},
-            {name: 'themeName', value: null},
-            {name: 'addingComment', value: null}
-        ];
+        self.module.attr('addingComment', null);
+        self.module.attr('showComments', false);
 
-        for (var i = 0, len = observables.length; i < len; i++) {
-            this[observables[i].name] = can.compute(observables[i].value);
+        var ageValue = [],
+            themeValue = [];
+
+        self.module.attr(options.moduleName + '.age').each(function (item) {
+            ageValue.push(item._id);
+        });
+
+        self.module.attr(options.moduleName + '.theme').each(function (item) {
+            themeValue.push(item._id);
+        });
+
+        self.module.attr('ageValue', ageValue);
+        self.module.attr('themeValue', themeValue);
+
+        if(!self.options.doc.attr('_id')) {
+            self.options.doc.attr('active', true);
         }
 
-        if (options.doc) {
-            if (options.doc.age) {
-                this.ageTitle(options.doc.age.title);
-                this.ageTitle(
-                    options.doc.age.icon
-                        ? options.doc.age.icon.title
-                        : undefined
-                );
-            }
-            if (options.doc.theme) {
-                this.themeName(options.doc.theme.name);
-            }
-        }
-
-        for (var i = 0, len = observables.length; i < len; i++) {
-            data[observables[i].name] = this[observables[i].name];
-        }
-
-        data['showComments'] = can.compute(false);
-
-        if(!this.options.doc.attr('_id')) {
-            this.options.doc.attr('active', true);
-        }
-
-        this.loadView(options.viewpath + options.viewName, data);
+        self.loadView(options.viewpath + options.viewName, self.module);
 	},
 
     ensureObject: function(obj, key, value = {}) {
@@ -84,33 +73,67 @@ export default Edit.extend({
     },
 
     '.currentAgeSelect change': function (el) {
-        var ages = el.find('option:selected').data('ages'),
-            newTitle = ages.attr('title'),
-            newFixture = ages.attr('icon.fixture');
 
-        this.ageTitle(newTitle);
-        this.ageFixture(newFixture);
+        var ages = this.module.attr(this.options.moduleName + '.age'),
+            newAgeValue = [];
+
+        if (ages instanceof can.List) {
+            ages.replace([]);
+        } else {
+            ages = new can.List;
+            this.module.attr(this.options.moduleName + '.age', ages);
+        }
+
+        el.find('option:selected').each(function () {
+            var data = $(this).data('ages');
+            ages.push({
+                _id: data.attr('_id'),
+                title: data.attr('title'),
+                fixture: data.attr('icon.fixture')
+            });
+            newAgeValue.push(data._id);
+        });
+
+        this.module.attr('ageValue', []);
+        this.module.attr('ageValue', newAgeValue);
     },
 
     '.currentThemeSelect change': function (el) {
-        var newVal = el.find('option:selected').data('themes').attr('name');
-        this.themeName(newVal);
+        var themes = this.module.attr(this.options.moduleName + '.theme');
+
+        if (themes instanceof can.List) {
+            themes.replace([]);
+        } else {
+            themes = new can.List;
+            this.module.attr(this.options.moduleName + '.theme', themes);
+        }
+
+        el.find('option:selected').each(function () {
+            var data = $(this).data('themes');
+            themes.push({
+                _id: data.attr('_id'),
+                name: data.attr('name')
+            });
+        });
     },
 
     '.removeAnswer click': function (el) {
-        if (confirm(this.options.removeAnswerMsg)) {
-            var item = $(el).parents('.answer').data('answer'),
-                answ = this.options.doc.attr('answer'),
-                index = answ.indexOf(item);
+        var self = this;
+        saConfirm('Удаление ответа', this.options.removeAnswerMsg, {closeOnConfirm: true}, function (isConfirmed) {
+            if (isConfirmed) {
+                var item = $(el).parents('.answer').data('answer'),
+                    answ = self.options.doc.attr('answer'),
+                    index = answ.indexOf(item);
 
-            if(index > -1) {
-                answ.splice(index, 1);
+                if(index > -1) {
+                    answ.splice(index, 1);
+                }
             }
-        }
+        });
     },
 
     '.addComment click': function (el) {
-        this.addingComment(!this.addingComment());
+        this.module.attr('addingComment', !this.module.attr('addingComment'));
     },
 
     '.confirmComment click': function (el) {
@@ -132,11 +155,11 @@ export default Edit.extend({
 
             doc.attr('answer').push(newComment);
 
-            self.addingComment(false);
+            self.module.attr('addingComment', false);
             wysiwyg.code('');
             saSuccess('Комментарий добавлен! Нажмите "Сохранить", чтобы сохранить добавленный комментарий.');
         }).fail(function (response) {
-            saError(response.err || response.responseText ? response.responseText.err : 'Произошла неизвестная ошибка.' );
+            saError(response.err || response.responseJSON ? response.responseJSON.err : 'Произошла неизвестная ошибка.' );
         });
     }
 });
