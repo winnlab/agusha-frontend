1610611e6b9a2557b44ecb375e24b9d3e1edec85
HydraOrc <hydra0@bigmir.net>
2014-11-02 04:38:49 +0200

    registered topper

--- js/app/user/modules/main/main.js
+++ js/app/user/modules/main/main.js
@@ -2,12 +2,25 @@ import Controller from 'controller'
 
 export default Controller.extend(
 	{
-        defaults: {
+		defaults: {
 			
-        }
-    }, {
+		}
+	}, {
+		variables: function() {
+			this.classname = 'active';
+			
+			this.tab_selectors = this.element.find('.tab_selector');
+		},
+		
 		after_init: function(data) {
 			
+		},
+		
+		'.tab click': function(el) {
+			var tab = el.data('tab');
+			
+			this.tab_selectors.removeClass(this.classname);
+			this.tab_selectors.filter('.' + tab).addClass(this.classname);
 		}
     }
 );
