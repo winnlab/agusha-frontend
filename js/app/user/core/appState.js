f055222e4256238e3713227683c1343718dc0444
Andrew Sygyda <andrew.sygyda@gmail.com>
2014-11-03 00:17:38 +0200

    Added popUp lib

--- js/app/user/core/appState.js
+++ js/app/user/core/appState.js
@@ -1,8 +1,10 @@
 import can from 'can/';
 import Viewport from 'lib/viewport/';
+import PopUp from 'lib/popUp/';
 
 var AppState = can.Map.extend({
-    viewport: new Viewport('body')
+    viewport: new Viewport('body'),
+    popUp: new PopUp('body')
 });
 
 export default new AppState();
