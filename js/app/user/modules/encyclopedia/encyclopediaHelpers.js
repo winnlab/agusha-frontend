b6b2eef1ff4ca1bb4d96fe3f2b7272df6bf7be19
Andrew Sygyda <andrew.sygyda@gmail.com>
2014-11-03 18:07:07 +0200

    Fix helper

--- js/app/user/modules/encyclopedia/encyclopediaHelpers.js
+++ js/app/user/modules/encyclopedia/encyclopediaHelpers.js
@@ -64,7 +64,7 @@ export default {
 
         img = entity.attr('image.' + size);
 
-        return img ? 'background-image: url("/img/uploads/"' + img + ');' : '';
+        return img ? 'background-image: url(/img/uploads/' + img + ');' : '';
     },
     is: function () {
         var options = arguments[arguments.length - 1],
