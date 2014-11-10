7d7579a9cffdf1de5d6629fba826055b9bd485b7
rastopyr <rastopyr@gmail.com[C>
2014-11-10 14:03:46 +0200

    Add fix for new user

--- js/app/user/modules/specialist/specialist.js
+++ js/app/user/modules/specialist/specialist.js
@@ -4,7 +4,7 @@ import encyclopediaHelpers from 'js/app/user/modules/encyclopedia/encyclopediaHe
 import appState from 'core/appState';
 
 var ViewModel = can.Map.extend({
-		isAuth: appState.attr('user.isAuth'),
+		isAuth: appState.attr('user.auth.isAuth'),
 		sort: 'asc',
 		filter: ''
 	});
@@ -99,7 +99,7 @@ export default Controller.extend(
 
 			this.select2();
 
-			appState.attr('user').delegate('user', 'set', function (ev, newVal) {
+			appState.attr('user').auth.delegate('isAuth', 'set', function (ev, newVal) {
 				that.data.attr('isAuth', newVal);
 				that.select2Options.width = '100%'
 				that.element.find('select.specialist_theme_select, select.specialist_age_select').select2(that.select2Options);
