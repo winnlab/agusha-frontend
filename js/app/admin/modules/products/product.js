77a069b4193fae0c61d32a0c19ffa262ed131f16
mshekera <max.shekera@gmail.com>
2014-11-19 21:09:04 +0200

    products module

--- js/app/admin/modules/products/product.js
+++ js/app/admin/modules/products/product.js
@@ -1,6 +1,8 @@
 import 'can/'
 import Edit from 'edit'
+import appState from 'appState'
 
+import _ from 'lodash'
 import 'summernote'
 import 'upload'
 
@@ -14,10 +16,42 @@ export default Edit.extend({
     }
 }, {
     init: function () {
-        Edit.prototype.init.call(this);
+        _.extend(this.options, options);
 
-        if(!this.options.doc.attr('_id')) {
-            this.options.doc.attr('active', true);
+        var self = this,
+            options = this.options;
+
+        self.module = new can.Map({
+            langs: appState.attr('langs')
+        });
+
+        if(!options.doc.attr('_id')) {
+            options.doc.attr('active', true);
+        }
+
+        self.ensureObject(options.doc, 'category', []);
+        self.ensureObject(options.doc, 'certificate', []);
+        
+        self.module.attr(options.moduleName, options.doc);
+
+        self.module.attr('ages', options.ages);
+        self.module.attr('cats', options.cats);
+        self.module.attr('certs', options.certs);
+
+        self.loadView(options.viewpath + options.viewName, self.module);
+    },
+
+    '.age-select change': function (el) {
+        var filtered = this.module.attr('ages').filter(function (item) {
+            return item._id.toString() == el.val();
+        });
+        console.log(filtered.attr('0.level'))
+        el.closest('.form-group').find('.age-level').val(filtered.attr('0.level'));
+    },
+
+    ensureObject: function(obj, key, value = {}) {
+        if (!_.isObject(obj.attr(key))) {
+            obj.attr(key, value);
         }
     }
 });
--- js/app/admin/modules/products/products.js
+++ js/app/admin/modules/products/products.js
@@ -1,6 +1,10 @@
 import 'can/'
 import List from 'list'
 
+import CertificateModel from 'js/app/admin/modules/certificates/certificateModel'
+import ProductAgeModel from 'js/app/admin/modules/productAges/productAgeModel'
+import ProductCategoryModel from 'js/app/admin/modules/productCategories/productCategoryModel'
+
 import ProductModel from 'js/app/admin/modules/products/productModel'
 import Product from 'js/app/admin/modules/products/product'
 
@@ -24,5 +28,51 @@ export default List.extend(
             parentData: '.products'
         }
     },
-    {}
+    {
+        init: function (el, options) {
+            var self = this;
+
+            List.prototype.init.apply(self, arguments);
+
+            self.module.attr('ages', new can.List);
+            self.module.attr('cats', new can.List);
+            self.module.attr('certs', new can.List);
+
+            ProductAgeModel.findAll({active: true}, function (docs) {
+                $.each(docs, function(i, doc) {
+                    self.module.attr('ages').push(doc);
+                });
+            });
+
+            ProductCategoryModel.findAll({active: true}, function (docs) {
+                $.each(docs, function(i, doc) {
+                    self.module.attr('cats').push(doc);
+                });
+            });
+
+            CertificateModel.findAll({}, function (docs) {
+                $.each(docs, function(i, doc) {
+                    self.module.attr('certs').push(doc);
+                });
+            });
+        },
+
+        initSetControl: function (area, doc, entity) {
+            area.html('');
+
+            var params = {
+                    ages: this.module.attr('ages'),
+                    cats: this.module.attr('cats'),
+                    certs: this.module.attr('certs'),
+                    doc,
+                    entity
+                };
+
+            if (this.options.EditHandle) {
+                this.options.EditHandle.destroy();
+            }
+            
+            this.options.EditHandle = new this.options.Edit(area, params);
+        }
+    }
 );
