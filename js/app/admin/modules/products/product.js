f26a29d3a2e9f50c206901e7d1dc7a7265d2acb5
HydraOrc <hydra0@bigmir.net>
2015-03-24 18:42:35 +0200

    removed 8 marta banner

--- js/app/admin/modules/products/product.js
+++ js/app/admin/modules/products/product.js
@@ -4,7 +4,6 @@ import appState from 'appState'
 
 import _ from 'lodash'
 import 'summernote'
-import 'upload'
 
 export default Edit.extend({
     defaults: {
--- js/app/admin/modules/products/productModel.js
+++ js/app/admin/modules/products/productModel.js
@@ -2,13 +2,13 @@ import baseModel from 'baseModel'
 import can from 'can/'
 
 export default can.Model.extend(
-    {
-        id: '_id',
-        resource: baseModel.chooseResource('/product'),
-        parseModel: baseModel.parseModel,
-        parseModels: baseModel.parseModels
-    }, {
-        uploaded: baseModel.simpleUploaded,
-        removeUploaded: baseModel.simpleRemoveUploaded
-    }
+	{
+		id: '_id',
+		resource: baseModel.chooseResource('/product'),
+		parseModel: baseModel.parseModel,
+		parseModels: baseModel.parseModels
+	}, {
+		uploaded: baseModel.simpleUploaded,
+		removeUploaded: baseModel.simpleRemoveUploaded
+	}
 );
