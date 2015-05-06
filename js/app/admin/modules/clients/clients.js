9eca03d94b91a72fffecd726156b87d22d0828a6
mshekera <max.shekera@gmail.com>
2015-05-06 22:10:47 +0300

    Added functionality for remote response aquisition

--- js/app/admin/modules/clients/clients.js
+++ js/app/admin/modules/clients/clients.js
@@ -7,6 +7,7 @@ import 'pagination';
 import moment from 'js/plugins/moment/min/moment.min';
 import 'swal';
 import 'daterangepicker';
+import 'bootstrap';
 
 import ClientModel from 'js/app/admin/modules/clients/clientModel';
 import Client from 'js/app/admin/modules/clients/client';
@@ -153,17 +154,101 @@ export default List.extend(
     },
 
     processError: function (err) {
-      var msg;
+      var msg = err.err, json;
 
-      if(err.errors && err.errors.title) {
+      if (!msg && err.errors && err.errors.title) {
         msg = err.errors.title.message;
       }
 
-      if(!msg) {
-        msg = err.message || err;
+      if (!msg && err.message) {
+        msg = err.message;
       }
 
+			if (!msg && err.responseJSON) {
+				json = err.responseJSON;
+        msg = _.isString(json.err) && json.err ||
+					json.data && json.data.message;
+      }
+
+			if (!msg && err.responseText) {
+				msg = err.responseText;
+			}
+
+			if (!msg && err.statusText) {
+				msg = err.statusText;
+			}
+
+			if (!msg) {
+				msg = err.toString();
+			}
+
+
       swal('Упс!', msg, 'error');
-    }
+    },
+
+		download: function (url, token) {
+			$('<form action="' + url +
+				'" method="post" enctype="multipart/form-data">' +
+				'<input type="hidden" name="token" value="' + token + '" />' +
+				'</form>'
+			)
+				.appendTo('body')
+				.submit()
+				.remove();
+		},
+
+		waitForExport: function () {
+			if (!_.isString(this.token)) {
+				return;
+			}
+
+			$.post('/admin/clients/export', {token: this.token})
+				.done(_.bind(function (body, data, response) {
+					if (response.status === 200) {
+						this.element.find('#export-popup').remove();
+						this.download('/admin/clients/downloadFile', this.token);
+					} else {
+						_.delay(_.bind(this.waitForExport, this), 2000);
+					}
+				}, this))
+				.fail(this.processError);
+		},
+
+		'#export-form submit': function (el, ev) {
+			var self = this;
+
+			ev.preventDefault();
+
+			$.post('/admin/clients/export', el.serialize())
+				.done(function (data) {
+					self.token = data;
+					self.showExportingPopup();
+					self.waitForExport();
+				})
+				.fail(this.processError);
+		},
+
+		showExportingPopup: function () {
+			if (this.element.find('#export-popup').size()) {
+				return;
+			}
+
+			this.element.append(
+				'<div id="export-popup" class="alert alert-warning alert-dismissible" role="alert" style="position:fixed;top:40%;left:40%;width:40%;">' +
+			    '<button type="button" class="close" data-dismiss="alert" aria-label="Close">' +
+			        '<span aria-hidden="true">&times;</span>' +
+			    '</button>' +
+			    '<strong>Данные экспортируются!</strong> Данная процедура может занять около 5 минут... Скачивание файла начнется автоматически.' +
+				'</div>'
+			);
+		},
+
+		'#t-export-last click': function () {
+			swal({
+				title: ":(",
+				text: "Данный функционал пока не реализован.",
+				type: "info"
+			});
+		}
   }
 );
