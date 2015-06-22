3bfba960bdd7cfb819365598d81fc9fd54670e01
Andrew Sygyda <andrew.sygyda@gmail.com>
2015-06-22 16:21:15 +0300

    fix born date

---
+++ dist/bundles/modules/news/news.js
@@ -0,0 +1,2 @@
+/*modules/news/news*/
+define("modules/news/news",["controller"],function(e){"use strict";e&&e.__esModule||(e={"default":e});var t=e["default"],n=t.extend({defaults:{}},{after_init:function(e){}});return{get default(){return n},__esModule:!0}});
